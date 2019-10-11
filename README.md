415e4a8c8c047aef1bb35313ab94b03baaaa0767
Dave Smith <da__smith@hotmail.com>
2019-10-11 10:00:00 +0100

    Tidying up

--- README.md
+++ README.md
@@ -1,16 +1,14 @@
 # ActiveGrids
 
-**A way to create pixel based grids yet make them responsive and fluid**
+**A way to do container queries — create pixel based grids yet make them responsive and fluid**
 
-The ActiveGrids default is to work to a grid of 60px min-width columns and dynamically add classes to .ags elements 
-dependent on their width, as the page loads. With your favourite JS library bind ActiveGrids to the window 
+Demo
+
+The ActiveGrids default is to work to a grid of 60px min-width columns and dynamically add classes to .ags elements
+dependent on their width, as the page loads. With your favourite JS library bind ActiveGrids to the window
 resize event and layouts will respond to browser resizing.
-
-**Status**: Not ready, an IE issue to be fixed. Otherwise works.
 
 See index.html for a demo/testing page of sorts.
 
 ... documentation to come and a better demo page or two
 ## Add ActiveGrids to your site
-
-
--- activegrids.src.js
+++ activegrids.src.js
@@ -6,11 +6,9 @@
 
 	'use strict';
 
-	// Definitions	
-	var
-	
-	// Defaults	
-	defaults = {
+	// Definitions
+	// Defaults
+	var defaults = {
 		minWidth: true, // false means maxWidth
 		width: 60, // number of px for minWidth or maxWidth
 		//customClass: '',
@@ -21,15 +19,15 @@
 		maxCols: 16, // to apply a practical limit to ags-lt classes, e.g., there will be no ags-lt1000 unless you want it
 		loading: false
 	},
-	
+
 	documentElement = document.documentElement,
 	elementsBySelectorIndex = {},
 	activeGridsjQuery = '//ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js',
 	batches = [],
 	currHeight,
 	currWidth,
-	
-	
+
+
 	// Mini compressions
 	QuerySelectorAll = 'querySelectorAll',
 	Length = 'length',
@@ -42,15 +40,15 @@
 
 	// ActiveGrids
 	activeGrids = function(optns) {
-	
+
 		var documentElementClientHeight,
 			documentElementClientWidth;
-		 
+
 		// Register an ActiveGrids batch
 		if (typeof optns !== 'undefined' && !optns.type) {
 
 			// Prepare an ActiveGrids batch
-			activeGridsjQuery = optns.jquery || activeGridsjQuery;			
+			activeGridsjQuery = optns.jquery || activeGridsjQuery;
 			// Merge batch options with the defaults
 			optns = optns || {};
 			for (var i in defaults) {
@@ -58,19 +56,19 @@
 					optns[i] = defaults[i];
 				}
 			}
-			
+
 			// Add the batch to the batches array
 			batches.push(optns);
-			
+
 		}
-		
+
 		// Else a window.resize event
 		else {
-			
+
 			// Refresh all batches if the DOM is ready and the window has been resized
 			documentElementClientWidth = documentElement.clientWidth;
 			documentElementClientHeight = documentElement.clientHeight;
-			
+
 			if (domReady) {
 				if (currHeight !== documentElementClientHeight || currWidth !== documentElementClientWidth) {
 					throttle(refreshBatches);
@@ -81,35 +79,36 @@
 		}
 		return false;
 	},
-	
-	
+
+
 	// Refresh batches
 	refreshBatches = function() {
-//console.log('refreshbatches '+batches.length);
-		for (var i = 0, batchesLength = batches[Length]; i < batchesLength; i++) {
+		var i, batchesLength;
+// console.log('refreshbatches '+batches.length);
+		for (i = 0, batchesLength = batches[Length]; i < batchesLength; i++) {
 			refreshBatch(batches[i]);
 		}
 	},
-	
-	
+
+
 	// Refresh a batch
 	refreshBatch = function(optns) {
-		
+		var optnsFind, elements, elementsLength;
+
 //console.log('refreshBatch, batches.length='+batches.length);
 
 		// Definitions
-		var
-			optnsFind = optns.find || '',
-		
+		optnsFind = optns.find || '';
+
 		// Get elements
 //console.log(document[QuerySelectorAll]);
-			elements = document[QuerySelectorAll](optnsFind),
-			elementsLength = elements[Length];
+		elements = document[QuerySelectorAll](optnsFind);
+		elementsLength = elements[Length];
 		//var elementsLength = (elements.length > 15) ? 15 : elements.length;
-		
+
 		//when doing a resize we want all the elements to change
 		//when loading up onload then we want it to be incremental
-		
+
 		// Reduce the number of times an element gets processed while the DOM is loading
 		elements = [].slice.call(elements, 0) || [];
 		if (!domIsReady) {
@@ -118,16 +117,16 @@
 			elementsBySelectorIndex[optnsFind] = elements;
 			elements = elementsLeftOver;
 		}
-		
+
 		elementsLength = elements[Length];
-		
+
 //console.log('elements.length:'+elements.length+' FOund:'+elementsFoundAlready[optnsFind].length+' leftover:'+elementsLeftOver.length+' optnsFind:'+optnsFind);
 		var $this,
 			optnsGridWidth = optns.width,
 			optnsGT = optns.gt,
 			optnsLT = optns.lt,
 			classPrepend = (optns.customClass || optns.defaultClass) + '-';
-			
+
 		// Process the elements
 		for (var i = 0; i < elementsLength; i++) {
 			$this = elements[i];
@@ -142,7 +141,7 @@
 				j,
 				k = 0,
 				diff;
-			
+
 			if (!optns.minWidth) {
 				numcols++;
 			}
@@ -153,13 +152,13 @@
 
 //console.log('cw:'+containerWidth+' cv:'+classValue);
 //console.log('k:'+classPrepend);
-			
+
 //console.log('cv after replace:'+classValue);
 			diff = (optnsGT === 'gte') ? 1 : 0;
 			for (j = 1; j < numcols + diff; j++) {
 				gt += ' ' + classPrepend + optnsGT + j;
 			}
-			
+
 			diff = (optnsLT === 'lt') ? 1 : 0;
 			for (j = numcols + diff; j <= optns.maxCols; j++) {
 				lt += ' ' + classPrepend + optnsLT + j;
@@ -168,72 +167,72 @@
 //console.log('cn:'+classValue);
 			gt = '';
 			lt = '';
-			
+
 			$this[ClassName] = classValue;
-			
+
 //console.log('count:'+count+' i:'+i+' numcols+diff:' +(numcols + diff) + ' optns.maxCols:' + optns.maxCols);
 //count++;
-			
+
 		}
 	},
-	
+
 	//count = 0,
 
 	// While loading ActiveGrids
 	timeoutID,
-	
+
 	agsWhileDOMLoading = function() {
 //console.log('whileloading');
 		refreshBatches();
 		timeoutID = window[SetTimeout](agsWhileDOMLoading, 76);
 	},
-	
-	
-	
+
+
+
 	// DOM Ready
 	domIsReady = false,
 	domReady = function() {
-		domIsReady = true;		
+		domIsReady = true;
 		window[ClearTimeout](timeoutID);
 		refreshBatches();
 		//var t = window[SetTimeout](refreshBatches, 200); // For IE 6, it wasn't getting the proper widths for the containers.
 //console.log(document[QuerySelectorAll]('.ags,.ags').length);
 	},
-	
-	
-	
-	
-	
-	
-	
-	
+
+
+
+
+
+
+
+
 	// Polyfill
 	polyfill = function() {
 //console.log('polyfill');
-		
+
 		var agsjQuery = jQuery.noConflict();
-	
+
 		// Polyfill, with thanks to branneman https://gist.github.com/1200441
 		document[QuerySelectorAll] = agsjQuery.find;
-		
+
 //		agsWhileDOMLoading();
-		
+
 		agsjQuery(function() {
 			domReady();
 //console.log('domREady');
 		});
 
 	},
-	
-	
-	
-	
+
+
+
+
 	// Obect Detection, which leads to Polyfill
 	objectDetection = function() {
 		// Polyfill
 		// Feature detect document.querySelectorsAll
 		if (document[QuerySelectorAll] && document[AddEventListener]) {
-//console.log('goodbrowser');
+// console.log('goodbrowser');
 			agsWhileDOMLoading();
 		}
 		else {
@@ -251,14 +250,14 @@
 						firstScript = document.getElementsByTagName(scriptTag)[0],
 						loaded,
 						ReadyState = 'readyState';
-						
+
 					//scriptElement.type = 'text/javascript'; // Is this line *really* needed
 					scriptElement.async = true;
 					scriptElement[Onreadystatechange] = scriptElement.onload = function(e) {
 						if (!loaded && (!this[ReadyState] || this[ReadyState] === 'complete' || this[ReadyState] === 'loaded')) {
 							this[Onreadystatechange] = null;
 							loaded = 1;
-							
+
 							polyfill();
 						}
 					};
@@ -275,7 +274,7 @@
 	},
 
 
-	
+
 	// Throttle function to prevent resize craziness, thanks to http://www.nczonline.net/blog/2007/11/30/the-throttle-function/
 	// Bastardised a bit as it wasn't working quote right for me. In Firefox at least it firing more than it needed to while resizing the window.
 	// The problem was resolved when the timeout ID was assigned as a variable in window object
@@ -290,18 +289,18 @@
 
 
 	// Problem with IE6 firing the resize event arbitarily
-	// snook http://snook.ca/archives/javascript/ie6_fires_onresize/	
-	
+	// snook http://snook.ca/archives/javascript/ie6_fires_onresize/
+
 	// Set DOM Ready event
 	if (document[AddEventListener]) {
 		document[AddEventListener]('DOMContentLoaded', domReady, false);
 	}
 
 	objectDetection();
-	
+
 	// Expose activeGrids
 	window.activeGrids = activeGrids;
 
-	
-	
+
+
 })(window, document);
